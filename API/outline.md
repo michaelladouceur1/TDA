470e14f051720c1e1c1a694a0293b2a7c8dff912
Michael LaDouceur <michaelladouceur1@gmail.com>
2020-03-04 16:11:57 -0600

    Started outline.md

---
+++ API/outline.md
@@ -0,0 +1,19 @@
+###Files
+- **tdapi**: Handles all calls to and from TDA API
+- **data**: Handles finding data in various locations (local, remote, etc.)
+- **db**: Setup and control of local database
+- **strategy**: Handles indicators 
+- **indicators**: Raw indicators are declared
+- **broker**: Handles buy/sell and positions
+- **utils**: Various utility functions and decorators
+
+##TDAPI
+
+##Data
+
+##DB
+
+##Strategy
+
+##Indicators
+- Indicators should try to return boolean values
--- API/strategy.py
+++ API/strategy.py
@@ -1,27 +1,33 @@
 from utils import *
 from data import *
+from graph import candle
 # from trader import Trader
 
 class Strategy():
 	def __init__(self,data):
 		period1 = 10
 		period2 = 50
-		period3 = 150
+		# period3 = 150
 		self.data = data
 		self.sma1 = sma(self.data,period1,'close')
 		self.sma2 = sma(self.data,period2,'close')
-		self.sma3 = sma(self.data,period3,'close')
+		# self.sma3 = sma(self.data,period3,'close')
 
 	def strategy(self,index):
-		sma1,sma2,sma3 = self.sma1[index],self.sma2[index],self.sma3[index]
-		if sma1 < sma2 and sma1 < sma3 and sma2 < sma3:
+		sma1,sma2 = self.sma1[index],self.sma2[index]
+		if sma1 < sma2:
 			self.trader.sell(10000)
-		elif sma1 > sma2 and sma1 > sma3 and sma2 > sma3:
+		elif sma1 > sma2:
 			self.trader.buy(10000)
 
 	def run(self):
 		for i in self.data.iterrows():
 			self.strategy(i[0])
 
+		candle(self.data,self.sma1,self.sma2,sell=sell,buy=buy)
+
 	def add_trader(self,trader):
 		self.trader = trader
+
+# and sma1 < sma3 and sma2 < sma3
+ # and sma1 > sma3 and sma2 > sma3
